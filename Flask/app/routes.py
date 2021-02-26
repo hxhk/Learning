13724586c0ad50e972f8ee06cd26febaee51b68f
hxhk <277970751@qq.com>
2021-02-26 15:41:41 +0800

    edit

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -1,10 +1,10 @@
 from app import app
-
+from flask import request,render_template,jsonify
 #routes
 @app.route('/')
 def hello():
     return "Hello,World!"
-  
+
 @app.route('/index')
 def index():
     return "This is an index page..."
@@ -16,3 +16,14 @@ def show_post(post_id):
 @app.route('/user/<username>')
 def show_user_profile(username):
     return 'User %s' % username
+    
+@app.route('/hello/<name>') #使用模板
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
+    
+@app.route('/get', methods = ["GET"])   #get
+def get_data():
+	name = request.args.get("name")
+	age = request.args.get("age")
+	return jsonify({'name': name, "age":age})
+	# return "name=%s, age=%d" % (name, age)
