e529dd6905d8d602bf37ea448fa500a76ca55fff
hxhk <277970751@qq.com>
2021-02-26 13:50:37 +0800

    routes.py edited

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -2,7 +2,17 @@ from app import app
 
 #routes
 @app.route('/')
+def hello():
+    return "Hello,World!"
+  
 @app.route('/index')
 def index():
-    return "Hello,World!"
-   
+    return "This is an index page..."
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post id %d' % post_id
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
